d28790f48ea0a1863343c86dd08d041058f9d39b
psakievich <psakiev@sandia.gov>
2020-11-20 21:01:33 -0700

    Update CTestNightlyScript.cmake

--- reg_tests/CTestNightlyScript.cmake
+++ reg_tests/CTestNightlyScript.cmake
@@ -16,6 +16,12 @@ else()
   message(FATAL_ERROR "You need to set the NALU_DIR variable. CMake will exit." )
 endif()
 
+if(NOT "${BUILD_DIR}" STREQUAL "")
+  message("BUILD_DIR is ${BUILD_DIR}")
+else()
+  message(FATAL_ERROR "You need to set the BUILD_DIR variable. CMake will exit.")
+endif()
+
 # -----------------------------------------------------------
 # -- Configure CTest
 # -----------------------------------------------------------
@@ -24,7 +30,7 @@ endif()
 set(CTEST_SITE "${HOST_NAME}")
 set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}${EXTRA_BUILD_NAME}")
 set(CTEST_SOURCE_DIRECTORY "${NALU_DIR}")
-set(CTEST_BINARY_DIRECTORY "${NALU_DIR}/build")
+set(CTEST_BINARY_DIRECTORY "${BUILD_DIR}")
 set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(MAKE NAMES make)
