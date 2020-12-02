9887a8e45bf8fa8a09505f45077054ee95bd4c8a
psakievich <psakiev@sandia.gov>
2020-12-02 12:29:17 -0700

    Fix typo

--- reg_tests/CTestNightlyScript.cmake
+++ reg_tests/CTestNightlyScript.cmake
@@ -33,7 +33,7 @@ set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(MAKE NAMES make)
 
-if(not "${CTEST_BUILD_FLAGS}" STREQUAL "") 
+if(NOT "${CTEST_BUILD_FLAGS}" STREQUAL "") 
 # Add parallelism capability to testing
 include(ProcessorCount)
 ProcessorCount(NP)
